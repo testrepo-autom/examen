382445a46b362962f860188c0435d2d16a7f46f8
Tester Automatizador 1 <testautomrepo@gmail.com>
2021-06-12 12:21:03 -0500

    Primer Examen

---
+++ src/test/java/Examen/TestExamen.java
@@ -0,0 +1,42 @@
+package Examen;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class TestExamen {
+
+    private WebDriver driver;
+
+    @Before
+    public void SetUp()
+    {
+        System.setProperty("webdriver.chrome.driver","src/main/resources/Drivers/ChromeDriver/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com.mx/");
+    }
+
+    @Test
+    public void Examen() throws InterruptedException
+    {
+        WebElement inputLogin = driver.findElement(By.xpath("//input[@name='q']"));
+        inputLogin.click();
+        inputLogin.clear();
+        inputLogin.sendKeys("Alexas");
+        inputLogin.submit();
+        Thread.sleep(3000);
+
+    }
+
+    @After
+    public void tearDown()
+    {
+        //Cierra la instancia del navegador
+        driver.quit();
+    }
+}
